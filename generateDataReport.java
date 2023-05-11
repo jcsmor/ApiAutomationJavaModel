7a147a2d86a18a778bb79ffe2a76582d5407be40
Joao Moreira <jdaimon@gmail.com>
2023-05-11 15:26:58 +0100

    RK-418 - ABP Payload Measurement

--- generateDataReport.java
+++ generateDataReport.java
@@ -67,7 +67,7 @@ class Scratch
       fw.write("*********** DATA TRANSFERRED WITH AA ON AND AA OFF ***********" + "\n\n\n");
       fw.write("TOTAL DATA TRANSFERRED FOR THE SAMPLED SITES WITH AA ON: " + sumON + "\n");
       fw.write("TOTAL DATA TRANSFERRED FOR THE SAMPLED SITES WITH AA OFF: " + sumOFF + "\n\n\n");
-      fw.write("TOTAL DIFERENCE: " + diference + "\n");
+      fw.write("TOTAL DIFFERENCE: " + diference + "\n");
       fw.write("PERCENTAGE GAIN: " + formattedString + "\n");
       fw.close();
     }
